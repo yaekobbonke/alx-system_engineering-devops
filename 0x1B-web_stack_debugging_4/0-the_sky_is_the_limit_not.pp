f43c5de48730f75a8569bfa72acc1a790334d5b8
yaekobbonke <yaekobbundure@gmail.com>
2023-08-19 15:58:44 +0000

    sky fixed

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,11 @@
 # Increases traffic an Nginx server can handle.
 
-exec { 'change workers':
-  command  => "sed -i 's/processes 4/processes 7/' /etc/nginx/nginx.conf && sudo service nginx restart",
-  provider => shell
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} 
+# Restart Nginx
+exec { 'nginx-restart':
+  command => 'sudo service nginx restart',
+  path    => '/etc/init.d/'
 }
